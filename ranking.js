18949b8b8809527bff0f4afe91c9394ec2bde7a2
enzogyoshi <enzogyoshi@gmail.com>
2026-09-06 14:29:54 -0300

    added table for rankings

--- ranking.js
+++ ranking.js
@@ -3,14 +3,23 @@ if (ranking.length === 0) {
     alert("ainda não tem nenhum jogador no ranking")
     window.location.href = "/index.html"
 }
-let listaRanking = document.getElementById("listaRanking")
+let listaRanking = document.getElementById("corpoRanking")
 ranking.sort((a, b) => b.pontos - a.pontos)
 for (const jogador of ranking) {
-    const resultado = document.createElement("div")
+    const numRank = ranking.indexOf(jogador)
+
+    const resultado = document.createElement("tr")
     resultado.classList = "resultado"
-    const resultadoNome = document.createElement("h4")
-    resultadoNome.textContent = "Nome: " + jogador.nome
-    const resultadoDificuldade = document.createElement("p")
+
+    const colocacaoRank = document.createElement("td")
+    colocacaoRank.textContent = 1 + numRank + "°"
+    colocacaoRank.classList = "colocacao"
+
+    const resultadoNome = document.createElement("td")
+    resultadoNome.textContent = jogador.nome
+    resultadoNome.classList = "nome-rank"
+
+    const resultadoDificuldade = document.createElement("td")
     if (jogador.dificuldade === "10") {
         resultadoDificuldade.textContent = "dificil"
     } else if (jogador.dificuldade === "15") {
@@ -18,13 +27,20 @@ for (const jogador of ranking) {
     } else {
         resultadoDificuldade.textContent = "facil"
     }
-    const resultadoTempo = document.createElement("p")
-    resultadoTempo.textContent = "tempo: " + jogador.tempo + "s"
-    const resultadoPontos = document.createElement("p")
-    resultadoPontos.textContent = "pontos: " + jogador.pontos 
+    resultadoDificuldade.classList = "dificuldade-rank"
+
+    const resultadoTempo = document.createElement("td")
+    resultadoTempo.textContent = jogador.tempo + "s"
+    resultadoTempo.classList = "tempo-rank"
+
+    const resultadoPontos = document.createElement("td")
+    resultadoPontos.textContent = jogador.pontos
+    resultadoPontos.classList = "pontos-rank" 
+
+    resultado.appendChild(colocacaoRank)
     resultado.appendChild(resultadoNome)
-    resultado.appendChild(resultadoDificuldade)
-    resultado.appendChild(resultadoTempo)
     resultado.appendChild(resultadoPontos)
+    resultado.appendChild(resultadoTempo)
+    resultado.appendChild(resultadoDificuldade)
     listaRanking.appendChild(resultado)
 }
